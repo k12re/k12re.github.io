dda7982e2cfdc0a5a55d5f6503e2324c40c9c893
Ken Thore <kenthore@live.no>
2022-12-14 16:27:41 +0100

    small tweaks and testing

--- docs/script.js
+++
@@ -1,9 +0,0 @@
-const circle = document.querySelector(".circle");
-const bodyContainer = document.querySelector("html");
-
-const scroll = () => {
-    const slideHeight = bodyContainer.clientHeight;
-    bodyContainer.scrollTo -= slideHeight;
-};
-
-circle.addEventListener("click", scroll)
